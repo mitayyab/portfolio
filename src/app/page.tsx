2cd1162184f1a8df01ce598f4752cfd57e5fe6a3
Muhammad Ibrahim Tayyab <ibrahimtayyab127@gmail.com>
2026-09-19 03:23:20 +0500

    feat(home): render selected work on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/components/hero/Hero";
 import { SiteHeader } from "@/components/layout/SiteHeader";
 import { SkipLink } from "@/components/layout/SkipLink";
+import { SelectedWork } from "@/components/work/SelectedWork";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <SiteHeader />
       <main id="content" tabIndex={-1}>
         <Hero />
+        <SelectedWork />
       </main>
     </>
   );
